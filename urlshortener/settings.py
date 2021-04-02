5cb45cca19e8c51ceb72a73a19d2ae5becc3a818
Jabez Borja <jabez.natsu@gmail.com>
2021-04-02 16:04:23 +0800

    Remove: Hide DJANGO_SECRET key

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'u+f=)3@2*a%u*=7erna*8yo&#&g5o0n*_z#wutj)n4fr+&)kc6'
+# SECRET_KEY is ignored in .gitignore, so please provide your own.
+with open(os.path.join("secretkey.txt")) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
