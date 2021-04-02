9c36f88e044e15bb42888f3e2d66d200acd95588
Jabez Borja <jabez.natsu@gmail.com>
2021-04-02 23:12:46 +0800

    Fix: Redirection bug

--- static/js/redirector.js
+++ static/js/redirector.js
@@ -2,16 +2,18 @@
 var countDown = document.getElementById("countdown");
 let count = 4;
 
-function count_decrement() {
+function count_decrement(url) {
     if (count == 1) {
-        window.location.href = "{{ url }}";
+        window.location.href = url;
         return;
     }
     
     count--;
     countDown.innerHTML = count;
 
-    setTimeout(count_decrement, 1000);
+    setTimeout(count_decrement, 1000, url);
 }
 
-setTimeout(count_decrement, 1000);
+function start_count(url) {
+    setTimeout(count_decrement, 1000, url);
+}
