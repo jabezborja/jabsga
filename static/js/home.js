1ba135057b618d52fde7c36fc1347ce8aaaa9553
Jabez Borja <jabez.natsu@gmail.com>
2021-04-03 00:07:16 +0800

    Add: Copied label when copy to clipboard

--- static/js/home.js
+++ static/js/home.js
@@ -1,4 +1,6 @@
+
 function copyText(url) {
+    let toolTipText = document.querySelector('.tooltiptext');
     var textToCopy = "https://jabsga.herokuapp.com/"+url;
 
     var myTemporaryInputElement = document.createElement("input");
@@ -6,9 +8,12 @@ function copyText(url) {
     myTemporaryInputElement.value = textToCopy;
 
     document.body.appendChild(myTemporaryInputElement);
+    toolTipText.innerHTML = "Copied!";
 
     myTemporaryInputElement.select();
     document.execCommand("Copy");
 
     document.body.removeChild(myTemporaryInputElement);
+
+    setTimeout(function() {toolTipText.innerHTML = "Copy to clipboard"}, 2000);
 }
--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECRET_KEY is ignored in .gitignore, so please provide your own.
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "a"
 
 HEROKU = ('ENV' in os.environ and os.environ['ENV'] == 'heroku')
 
