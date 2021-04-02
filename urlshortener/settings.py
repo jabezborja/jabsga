17f07da641c71ce3bae98cc830919cc74deac19b
Jabez Borja <jabez.natsu@gmail.com>
2021-04-02 16:11:26 +0800

    Fix: Gitignore error

--- urlshortener/settings.py
+++ urlshortener/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECRET_KEY is ignored in .gitignore, so please provide your own.
-with open(os.path.join("secretkey.txt")) as f:
+with open(os.path.join(".env")) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
